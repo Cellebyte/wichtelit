fcfec9f378f873f04eaa13bbf2aa57a20eda6b18
Cellebyte <marcel@fest4.eu>
2020-04-11 15:46:13 +0200

    Added error handing and added a created view

--- wichtelit/wichtelit/urls.py
+++ wichtelit/wichtelit/urls.py
@@ -21,9 +21,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomeView.as_view(), name='home'),
     path('impressum/', views.ImprintView.as_view(), name='impressum'),
-    path('wichteln/<uuid:wichtelgruppe_id>', views.MemberFormView.as_view(), name='memberform'),
+    path(
+        'wichteln/<uuid:wichtelgruppe_id>',
+        views.MemberFormView.as_view(),
+        name='memberform'
+    ),
     path('wichteln/', views.GruppenView.as_view(), name='wichteln'),
     # path('members/', views.MembersView.as_view(), name='members'),
     # path('members', views.return_member, name='members'),
-    # path('wichteln/created/', view.)
+    path('wichteln/<uuid:wichtelgruppe_id>/created', views.CreatedMemberView.as_view(), name='created')
 ]
--- wichtelit/wichtelit/views.py
+++ wichtelit/wichtelit/views.py
@@ -1,10 +1,14 @@
+from datetime import date
+
 from django import http
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
 
 from wichtelit.forms import GruppenForm, MemberForm
 from wichtelit.models import Wichtelgruppe, Wichtelmember
-from django.views.generic.edit import FormView
-from django.views.generic.base import TemplateView
-from django.http import HttpResponse
 
 
 class MyTemplateView(TemplateView):
@@ -25,6 +29,21 @@ class ImprintView(MyTemplateView):
     template_name = 'impressum.html'
 
 
+class CreatedMemberView(MyTemplateView):
+    name = 'created'
+    template_name = 'success_MemberForm.html'
+
+    def get(self, request, *args, **kwargs):
+        try:
+            self.wichtelgruppe = MemberFormView.get_group(
+                wichtelgruppe_id=self.kwargs.pop('wichtelgruppe_id')
+            )
+        except Wichtelgruppe.DoesNotExist:
+            return render(request, 'error_NoMemberForm.html')
+        kwargs['wichtelgruppe'] = self.wichtelgruppe
+        return super().get(request, *args, **kwargs)
+
+
 class GruppenView(FormView):
     template_name = 'form_GruppenForm.html'
     form_class = GruppenForm
@@ -34,7 +53,6 @@ class GruppenView(FormView):
         self.object.budget = 0
         self.object.save()
         return super().form_valid(form)
-        # return http.HttpResponseRedirect(self.get_success_url())
 
     def get_success_url(self):
         return str(self.object.id)
@@ -47,27 +65,42 @@ class GruppenView(FormView):
 #         content.append(f"{member.emailAddress}, {member.budget}")
 #     return HttpResponse(content=content, status=200)
 
-
 class MemberFormView(FormView):
     template_name = 'form_MemberForm.html'
     form_class = MemberForm
 
-    def form_valid(self, form):
+    @staticmethod
+    def get_group(wichtelgruppe_id):
+        return Wichtelgruppe.objects.get(id=wichtelgruppe_id)
+
+    def check_group_available(self, request):
         try:
-            # Hier hole ich mir die Wichtelgruppe aus der Datenbank.
-            wichtelgruppe_id = self.kwargs.pop('wichtelgruppe_id')
-            wichtelgruppe = Wichtelgruppe.objects.get(id=wichtelgruppe_id)
-            # Hier erstelle ich aus der Eingabemaske das potentielle Objekt.
-            self.object = form.save(commit=False)
-            # Nun die wichtelgruppe von oben hinzufügen
-            self.object.wichtelgruppe = wichtelgruppe
-            wichtelgruppe.budget = wichtelgruppe.budget + form.cleaned_data['budget']
-            self.object.save()
-            wichtelgruppe.save()
-            return http.HttpResponse(
-                f"Viel Spaß beim wichteln. Am {self.object.wichtelgruppe.wichteldatum} wird gewichtelt."
+            self.wichtelgruppe = MemberFormView.get_group(
+                wichtelgruppe_id=self.kwargs.pop('wichtelgruppe_id')
             )
         except Wichtelgruppe.DoesNotExist:
-            return http.HttpResponseNotFound(
-                "Die Wichtelgruppe scheint es nicht mehr zu geben."
-            )
+            return render(request, 'error_NoMemberForm.html', status=400)
+        if date.today() >= self.wichtelgruppe.ablaufdatum:
+            return render(request, 'error_ClosedMemberForm.html', status=403)
+        return False
+
+    def get(self, request, *args, **kwargs):
+        # Wenn das erste None returned dann wird erst das zweite ausgeführt.
+        return self.check_group_available(request) or super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.check_group_available(request) or super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        # Hier hole ich mir die Wichtelgruppe aus der Datenbank.
+        # Hier erstelle ich aus der Eingabemaske das potentielle Objekt.
+        self.object = form.save(commit=False)
+        # Nun die wichtelgruppe von oben hinzufügen
+        self.object.wichtelgruppe = self.wichtelgruppe
+        self.wichtelgruppe.budget = self.wichtelgruppe.budget + form.cleaned_data['budget']
+        self.object.save()
+        self.wichtelgruppe.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return f'{str(self.wichtelgruppe.id)}/created'
