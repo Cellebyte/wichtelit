715c6ee51dbd73f46f7bbf9636c0a74ad4f4ac71
Cellebyte <marcel.fest@live.de>
2020-04-14 11:05:32 +0200

    removed the admin site

--- wichtelit/wichtelit/urls.py
+++ wichtelit/wichtelit/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.HomeView.as_view(), name='home'),
     path('impressum/', views.ImprintView.as_view(), name='impressum'),
     path(
