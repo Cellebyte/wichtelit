ecb883f33588686bc929856ffa65ab6b30471562
Peter Fest <Hann17-1-Stud05@EGSGOE01.local>
2020-05-01 18:21:42 +0200

    Kannst du das email feature mal ausprobieren?

--- wichtelit/wichtelit/logic/email.py
+++ wichtelit/wichtelit/logic/email.py
@@ -1,12 +1,14 @@
-from wichtelit.models import Wichtelgruppe, Wichtelmember
-from django.http import HttpResponse
+from wichtelit.models import Wichtelmember
+from django.core.mail import send_mail
+
+
 def emailing(foo):
     for member in Wichtelmember.objects.all():
-        send_messages(
+        send_mail(
             'Erinnernung an das Wichteln.',
-            f'Dein Wichtelpartner ist: {member.wichtelmember.wichtelpartner}'+
-            f' Das Budget liegt bei {member.wichtelmember.}' +
-            f' Am {member.wichtelgruppe.wichteldatum} wird gewichtelt.',
+            f'Dein Wichtelpartner ist: {member.wichtelmember.wichtelpartner}\n' +
+            f'Das Budget liegt bei {member.wichtelmember.wichtelgruppe.budget}\n' +
+            f'Am {member.wichtelmember.wichtelgruppe.wichteldatum} wird gewichtelt.\n',
             f'wichtelit@1kbyte.de',
             [f'{member.wichtelmember.emailAdresse}'],
             fail_silently=False,
