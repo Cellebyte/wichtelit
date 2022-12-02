b6b19f24360ad2a4e52c1a32534a7f061b97bf20
Pietsupply <59282206+Pietsupply@users.noreply.github.com>
2022-12-02 13:17:54 +0100

    Update lostopf.py
    Added a scrambling method
    Added scrambling of Lostopf in every iteration

--- wichtelit/wichtelit/logic/lostopf.py
+++ wichtelit/wichtelit/logic/lostopf.py
@@ -1,3 +1,4 @@
+import random
 import logging
 import secrets
 from typing import List
@@ -10,17 +11,21 @@ logger = logging.getLogger(__name__)
 class LosTopf(object):
 
     @staticmethod
+    def scrambled(original)
+        dest = original[:]
+        random.shuffle(dest)
+        return dest
+
     def ziehen(members: List[Wichtelmember], lostopf: List[Wichtelmember]):
         for index in range(len(members)):
+            lostopf = scrambled(lostopf)
             not_in_list = False
             loszieher = members[index]
             try:
                 lostopf.remove(loszieher)
             except ValueError:
                 not_in_list = True
-            loszieher.wichtelpartner = secrets.choice(
-                lostopf
-            )
+            loszieher.wichtelpartner = secrets.choice(lostopf)
             logger.debug(
                 f'{loszieher.emailAdresse} --> {loszieher.wichtelpartner.emailAdresse}')
             loszieher.status = Status.GEWÜRFELT
